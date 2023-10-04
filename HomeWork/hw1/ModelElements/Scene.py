5e6b439e8f74a5a1ebcd1af3d383c679a76531c1
vit21513 <vit21513@yandex.ru>
2023-10-04 19:48:25 +0300

    home1ver2

--- HomeWork/hw1/ModelElements/Scene.py
+++ HomeWork/hw1/ModelElements/Scene.py
@@ -1,11 +1,19 @@
+from HomeWork.hw1.ModelElements import Camera
 from HomeWork.hw1.ModelElements.Flash import Flash
 from HomeWork.hw1.ModelElements.PoligonalModel import PoligonalModel
 
 
 class Scene:
-    id: int
-    models: list[PoligonalModel]
-    flashes: list[Flash]
+    cameras: list[Camera]
+
+    def __init__(self, id: int, models: list[PoligonalModel], flashes: list[Flash], cameras: list[Camera]):
+        self.id = id
+        if len(models) > 0:
+            self.models = models
+        else:
+            raise Exception('должна быть хоть одна модель')
+        self.flashes = flashes
+        self.cameras = cameras
 
     def method1(self, obj: object):
         return obj
