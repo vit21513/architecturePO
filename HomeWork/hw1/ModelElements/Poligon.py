7efaa7d688c44b14bd77950c595bca3091712385
vit21513 <vit21513@yandex.ru>
2023-10-05 09:52:51 +0300

    р1

--- HomeWork/hw1/ModelElements/Poligon.py
+++ HomeWork/hw1/ModelElements/Poligon.py
@@ -2,4 +2,7 @@ from HomeWork.hw1.ModelElements.Point3D import Point3D
 
 
 class Poligon:
-    points = list[Point3D]
+    points: list[Point3D]
+
+    def __init__(self, points: list[Point3D]):
+        self.points = points
--- HomeWork/hw1/ModelElements/PoligonalModel.py
+++ HomeWork/hw1/ModelElements/PoligonalModel.py
@@ -1,3 +1,4 @@
+from HomeWork.hw1.ModelElements.Point3D import Point3D
 from HomeWork.hw1.ModelElements.Poligon import Poligon
 from HomeWork.hw1.ModelElements.Texture import Texture
 
@@ -6,6 +7,10 @@ class PoligonalModel:
 
     poligons: list[Poligon]
     textures: list[Texture]
+
+    def __init__(self,textures: list[Texture]):
+        self.textures = textures
+        PoligonalModel.poligons.append(Poligon(Point3D()))
 
 
 
